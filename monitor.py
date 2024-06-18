6a05ec4c9600472e54109532dfa687c4024ae544
Sb4m98 <150377112+Sb4m98@users.noreply.github.com>
2024-06-18 23:12:09 +0200

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -4,6 +4,7 @@ from azure.keyvault.secrets import SecretClient
 import shodan
 import smtplib
 from email.mime.text import MIMEText
+from pymongo import MongoClient
 
 # Recupera i segreti dalle variabili d'ambiente
 API_KEY = os.getenv('SHODAN_API_KEY')
@@ -18,7 +19,12 @@ TO_EMAIL = os.getenv('TO_EMAIL_SECRET')
 print(TO_EMAIL)
 SMTP_SERVER = os.getenv('SMTP_SERVER_SECRET')
 print(SMTP_SERVER)
-
+DB_URI = os.getenv('DB_URI')
+print(DB_URI)
+DB_NAME = os.getenv('DB_NAME')
+print(DB_NAME)
+COLLECTION_NAME = os.getenv('COLLECTION_NAME')
+print(COLLECTION_NAME)
 api = shodan.Shodan(API_KEY)
 
 def invia_notifica(oggetto, corpo):
@@ -35,6 +41,14 @@ def invia_notifica(oggetto, corpo):
         print("Email inviata con successo.")
     except smtplib.SMTPException as e:
         print(f"Errore nell'invio dell'email: {e}")
+# Funzione per salvare i dati nel database
+def salva_dati(dispositivo):
+    try:
+        collection.insert_one(dispositivo)
+        print(f"Dati salvati per il dispositivo: {dispositivo['ip']}")
+    except Exception as e:
+        print(f"Errore nel salvataggio dei dati: {e}")
+
 
 def ricerca_dispositivi_vulnerabili(query):
     try:
@@ -66,6 +80,7 @@ def monitoraggio(query):
     for dispositivo in vulnerabili:
         corpo_notifica = f"IP: {dispositivo['ip']}\nPorta: {dispositivo['port']}\nVulnerabilità: {dispositivo['vulnerabilita']}\nDati: {dispositivo['dati']}"
         invia_notifica("Allerta Shodan: Dispositivo Vulnerabile Trovato", corpo_notifica)
+        salva_dati(dispositivo)
 
 # Esegui il monitoraggio per una query specifica
 query = 'country:"IT" city:"Roma" product:"webcam"'
