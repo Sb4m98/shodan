22da3f1ce862be4164bcb4c648b8b07e4c71c87d
Sb4m98 <150377112+Sb4m98@users.noreply.github.com>
2024-07-15 12:34:14 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template_string, jsonify
+from flask import Flask, request, render_template_string, jsonify, redirect
 import monitor
 
 app = Flask(__name__)
